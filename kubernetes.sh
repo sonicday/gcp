d80e02d0d6097e38467b157d3c49476423600328
sonicday <16403738+sonicday@users.noreply.github.com>
2019-11-06 13:29:39 +0100

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -2,3 +2,8 @@
 gcloud container clusters get-credentials cluster-name --zone europe-west3-a --project my-project-123456
 # make service public
 kubectl patch svc "prometheus-1-grafana"  -p '{"spec": {"type": "LoadBalancer"}}'                                     
+# create cluster
+gcloud container clusters create mycluster \
+--num-nodes 2 \
+--machine-type n1-standard-2 \
+--scopes "https://www.googleapis.com/auth/source.read_write,cloud-platform"
